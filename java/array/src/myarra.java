b741ff68fc878dda9f8664568af612babc4d47b3
MapanaGit <mkasi.mapana@gmail.com>
2024-10-23 04:09:24 +0200

    Add files via upload

---
+++ java/array/src/myarra.java
@@ -0,0 +1,11 @@
+
+//java libraries
+import java.util.Arrays;
+public class myarra {
+    public static void main(String[] args){
+        //store data in single variable
+        String[] cars = {"Ford","BMW","Toyota","Mercedez Benz"};
+        //identify the Toyota index
+        System.out.println(cars.length);  
+    }
+}
