55fe7762244cf6a3c8733070ce111ce82570d3de
MapanaGit <mkasi.mapana@gmail.com>
2024-10-23 04:15:15 +0200

    Add files via upload

---
+++ java/data types/src/java.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class java {
+    public static void main(String[] args){
+    //integrer datatype
+    int x=36;
+    int y=1;
+    
+    System.out.println(x+y);
+    
+    String name,surname;
+    double price;
+    char anytext;
+    
+    }
+}
